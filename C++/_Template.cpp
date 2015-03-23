943a7615716bf35944273d516b15828641f30742
Lane <zhanglintc623@gmail.com>
2015-03-23 20:54:25 +0800

    _Template

---
+++ C++/_Template.cpp
@@ -0,0 +1,15 @@
+// Problem Name Here
+// for leetcode problems
+// 2015.03.23 by zhanglin
+
+// Problem:
+// Problem description here
+
+class Solution {
+public:
+    void Function() {
+        ;
+    }
+};
+
+
---
+++ Java/_Template.java
@@ -0,0 +1,14 @@
+// Problem Name Here
+// for leetcode problems
+// 2015.03.23 by zhanglin
+
+// Problem:
+// Problem description here
+
+public class Solution {
+    void Function() {
+        ;
+    }
+}
+
+
