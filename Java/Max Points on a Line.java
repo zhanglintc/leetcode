35a4acb66d0561241213034288c14de0eedc0034
Lane <zhanglintc623@gmail.com>
2015-03-24 23:02:28 +0800

    INIT

---
+++ Java/Max Points on a Line.java
@@ -0,0 +1,23 @@
+// Max Points on a Line
+// for leetcode problems
+// 2015.03.24 by zhanglin
+
+// Problem:
+// Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    public int maxPoints(Point[] points) {
+        ;
+    }
+}
+
+
