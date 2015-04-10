eaf8455ec2a7ab3ea28cc47f0f648bf8f3b1d148
Lane <zhanglintc623@gmail.com>
2015-04-10 22:30:52 +0800

    AC

--- Python/Binary Tree Postorder Traversal.py
+++ Python/Binary Tree Postorder Traversal.py
@@ -29,19 +29,14 @@ class Solution:
     def postorderTraversal(self, root):
         lst = []
         self.helper(root, lst)
-        return lst        
+        return lst
 
     def helper(self, root, lst):
-        if root != None and root != []:
-            self.helper(root.left,  lst)
-            self.helper(root.right, lst)
-            lst.append(root.val)
+        if not root:
+            return
 
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+        self.helper(root.left,  lst)
+        self.helper(root.right, lst)
+        lst.append(root.val)
 
 
