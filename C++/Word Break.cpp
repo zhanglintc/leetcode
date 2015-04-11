138b152767938db0395201e612bd5507733e34d6
Lane <zhanglintc623@gmail.com>
2015-04-11 12:36:34 +0800

    INIT

---
+++ C++/Word Break.cpp
@@ -0,0 +1,22 @@
+// Word Break
+// for leetcode problems
+// 2015.04.11 by zhanglin
+
+// Problem:
+// Given a string s and a dictionary of words dict,
+// determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// For example, given
+// s = "leetcode",
+// dict = ["leet", "code"].
+
+// Return true because "leetcode" can be segmented as "leet code".
+
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        ;
+    }
+};
+
+
