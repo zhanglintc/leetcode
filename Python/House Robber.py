8e21d87930e804283f49543dca7a5277f6f4a148
Lane <zhanglintc623@gmail.com>
2015-04-16 18:17:21 +0800

    INIT

---
+++ Python/House Robber.py
@@ -0,0 +1,24 @@
+# House Robber
+# for leetcode problems
+# 2015.04.16 by zhanglin
+
+# Problem:
+# You are a professional robber planning to rob houses along a street.
+# Each house has a certain amount of money stashed, the only constraint stopping you from
+# robbing each of them is that adjacent houses have security system connected
+# and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given a list of non-negative integers representing the amount of money of each house,
+# determine the maximum amount of money you can rob tonight without alerting the police.
+
+# Credits:
+# Special thanks to @ifanchu for adding this problem and creating all test cases.
+# Also thanks to @ts for adding additional test cases.
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        pass
+
+
