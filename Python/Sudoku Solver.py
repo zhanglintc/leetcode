2178709a2934ab95d887c4045293cdbb71eaf1e7
Lane <zhanglintc623@gmail.com>
2015-04-16 16:59:43 +0800

    MIDDLE

--- Python/Sudoku Solver.py
+++ Python/Sudoku Solver.py
@@ -75,7 +75,54 @@ class Solution:
         # all well, return True
         return True
 
-    def solveSudoku(self, board):
-        pass
 
+    def solveSudoku(self, board):
+        for row in range(9):
+            for line in range(9):
+                if board[row][line] == '.':
+                    for i in range(1, 10):
+                        board[row][line] = str(i)
+                        if self.isValidSudoku(board) and self.solveSudoku(board):
+                            return True
+                        board[row][line] = '.'
+                    return False
+        return True
+    '''
+
+    def solveSudoku(self, board):
+        def isValid(x,y):
+            tmp=board[x][y]; board[x][y]='D'
+            for i in range(9):
+                if board[i][y]==tmp: return False
+            for i in range(9):
+                if board[x][i]==tmp: return False
+            for i in range(3):
+                for j in range(3):
+                    if board[(x/3)*3+i][(y/3)*3+j]==tmp: return False
+            board[x][y]=tmp
+            return True
+        def dfs(board):
+            for i in range(9):
+                for j in range(9):
+                    if board[i][j]=='.':
+                        for k in '123456789':
+                            board[i][j]=k
+                            if isValid(i,j) and dfs(board):
+                                return True
+                            board[i][j]='.'
+                        return False
+            return True
+        dfs(board)
+    '''
+raw_board = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
+board = []
+for row in raw_board:
+    new_row = []
+    for c in row:
+        new_row.append(c)
+    board.append(new_row[::])
+
+s = Solution()
+s.solveSudoku(board)
+print board
 
