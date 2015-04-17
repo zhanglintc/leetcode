048e96125615173b1e523321fec67a9a0f6da087
Lane <zhanglintc623@gmail.com>
2015-04-17 22:36:50 +0800

    INIT

---
+++ Java/Insertion Sort List.java
@@ -0,0 +1,28 @@
+// Insertion Sort List
+// for leetcode problems
+// 2015.04.17 by zhanglin
+
+// Problem Link:
+// https://leetcode.com/problems/insertion-sort-list/
+
+// Problem:
+// Sort a linked list using insertion sort.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ;
+    }
+}
+
+
