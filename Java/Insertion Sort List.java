0ef2ea46ea7215f534116cae024a0f2142e487f1
Lane <zhanglintc623@gmail.com>
2015-04-18 22:20:17 +0800

    AC

--- Java/Insertion Sort List.java
+++ Java/Insertion Sort List.java
@@ -21,7 +21,36 @@
  */
 public class Solution {
     public ListNode insertionSortList(ListNode head) {
-        ;
+        if(head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode insertPos = null;
+        ListNode tobeMoved = null;
+
+        ListNode current = head;
+        while(current.next != null) {
+            if(current.val < current.next.val) {
+                current = current.next;
+            }
+
+            else {
+                insertPos = dummy;
+                while(insertPos.next.val < current.next.val) {
+                    insertPos = insertPos.next;
+                }
+
+                tobeMoved = current.next;
+                current.next = tobeMoved.next;
+                tobeMoved.next = insertPos.next;
+                insertPos.next = tobeMoved;
+            }
+        }
+
+        return dummy.next;
     }
 }
 
