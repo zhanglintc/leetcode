496606c28f84fd2862e08a58b19d146ae2a789ad
Lane <zhanglintc623@gmail.com>
2015-04-20 21:10:57 +0800

    INIT

---
+++ Python/Binary Tree Right Side View.py
@@ -0,0 +1,36 @@
+# Binary Tree Right Side View
+# for leetcode problems
+# 2015.04.20 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Problem:
+# Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+# For example:
+# Given the following binary tree,
+#    1            <---
+#  /   \
+# 2     3         <---
+#  \     \
+#   5     4       <---
+# You should return [1, 3, 4].
+
+# Credits:
+# Special thanks to @amrsaqr for adding this problem and creating all test cases.
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def rightSideView(self, root):
+        pass
+
+
