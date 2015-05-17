261f860826bc9293c603dd40c20d110dbfe39735
Lane <zhanglintc623@gmail.com>
2015-05-17 22:31:10 +0800

    INIT

---
+++ Python/Candy.py
@@ -0,0 +1,23 @@
+# Candy
+# for leetcode problems
+# 2015.05.17 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/candy/
+
+# Problem:
+# There are N children standing in a line. Each child is assigned a rating value.
+
+# You are giving candies to these children subjected to the following requirements:
+
+# Each child must have at least one candy.
+# Children with a higher rating get more candies than their neighbors.
+# What is the minimum candies you must give?
+
+class Solution:
+    # @param {integer[]} ratings
+    # @return {integer}
+    def candy(self, ratings):
+        pass
+
+
