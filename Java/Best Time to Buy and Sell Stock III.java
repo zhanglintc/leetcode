b48b9e9c7f784bf003e65bdb6f09053659dccdbd
Lane <zhanglintc623@gmail.com>
2015-05-17 16:58:32 +0800

    INIT

---
+++ Java/Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,22 @@
+// Best Time to Buy and Sell Stock III
+// for leetcode problems
+// 2015.05.17 by zhanglin
+
+// Problem Link:
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+
+// Problem:
+// Say you have an array for which the i[th] element is the price of a given stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete at most TWO transactions.
+
+// Note:
+// You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        ;
+    }
+}
+
+
