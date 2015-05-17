29ded9e5391c95ed27e0a64628cc3cb83c94ac7e
Lane <zhanglintc623@gmail.com>
2015-05-17 16:55:22 +0800

    AC

--- C++/Best Time to Buy and Sell Stock III.cpp
+++ C++/Best Time to Buy and Sell Stock III.cpp
@@ -15,37 +15,36 @@
 
 class Solution {
 public:
-    int maxProfit_helper(vector<int> v) {
+    int maxProfit(vector<int> &prices) {
         int profit = 0;
 
-        if(v.size() == 0) {
+        if(prices.size() == 0) {
             return profit;
         }
 
-        int lowest = v[0];
+        int  first[prices.size()] = {};
+        int second[prices.size()] = {};
 
-        for(int i = 0; i < v.size(); i++) {
-            lowest = min(lowest, v[i]);
-            profit = max(profit, v[i] - lowest);
+        // O(n) time to find maximum profit for first transaction
+        int lowest = prices[0];
+        for(int i = 0; i < prices.size(); i++) {
+            lowest = prices[i] < lowest ? prices[i] : lowest;
+            first[i] = prices[i] - lowest > first[i - 1] ? prices[i] - lowest : first[i - 1];
+        }
+
+        // O(n) time to find maximum profit for second transaction
+        int highest = prices[prices.size() - 1];
+        for(int i = prices.size() - 2; i >= 0; i--) {
+            highest = highest > prices[i] ? highest : prices[i];
+            second[i] = highest - prices[i] > second[i + 1] ? highest - prices[i] : second[i + 1];
+        }
+
+        // O(n) time to find maximum profit
+        for(int i = 0; i < prices.size(); i++) {
+            profit = profit > first[i] + second[i] ? profit : first[i] + second[i];
         }
 
         return profit;
-    }
-
-    int maxProfit(vector<int> &prices) {
-        vector<int> v;
-        int profit = 0;
-        int left;
-        int right;
-
-        for(int i = 0; i < prices.size(); i++) {
-            v.assign(prices[0], prices[i]);
-            left  = maxProfit_helper(v);
-            v.assign(prices[i], prices[prices.size()]);
-            right = maxProfit_helper(v); 
-        }
-
-        profit = max(profit, left + right);
     }
 };
 
