2b3f89db8b77f9578b5df91bbf1012f162b89b0e
Lane <zhanglintc623@gmail.com>
2015-05-18 16:08:48 +0800

    AC

--- Python/Candy.py
+++ Python/Candy.py
@@ -18,6 +18,16 @@ class Solution:
     # @param {integer[]} ratings
     # @return {integer}
     def candy(self, ratings):
-        pass
+        candies = [1 for i in range(len(ratings))]
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
+                candies[i] = candies[i + 1] + 1
+
+        return sum(candies)
 
 
