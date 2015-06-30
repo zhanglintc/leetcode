9591b69f4418d34c171c95a3d40e6ca035f5cd1a
Lane <zhanglintc623@gmail.com>
2015-06-30 22:34:26 +0800

    INIT

---
+++ Python/Contains Duplicate.py
@@ -0,0 +1,19 @@
+# Contains Duplicate
+# for leetcode problems
+# 2015.06.30 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/contains-duplicate/
+
+# Problem:
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice in the array,
+# and it should return false if every element is distinct.
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean}
+    def containsDuplicate(self, nums):
+        pass
+
+
