0c88e64fa56d5dc7ee082bc9006f9a2bd4870f70
Lane <zhanglintc623@gmail.com>
2015-07-01 10:47:32 +0800

    INIT

---
+++ Python/Reverse Linked List.py
@@ -0,0 +1,22 @@
+# Reverse Linked List
+# for leetcode problems
+# 2015.07.01 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Problem:
+# Reverse a singly linked list.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        pass
+
