0fa46b441ad9bfc5d2a2052daae907dd74093c77
Lane <zhanglintc623@gmail.com>
2015-07-01 16:48:18 +0800

    INIT

---
+++ Shell/Transpose File.sh
@@ -0,0 +1,26 @@
+# Transpose File
+# for leetcode problems
+# 2015.07.01 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/transpose-file/
+
+# Problem:
+# Given a text file file.txt, transpose its content.
+
+# You may assume that each row has the same number of columns and each field is separated by the ' ' character.
+
+# For example, if file.txt has the following content:
+
+# name age
+# alice 21
+# ryan 30
+# Output the following:
+
+# name alice ryan
+# age 21 30
+
+# Read from the file file.txt and print its transposed content to stdout.
+Your Shell Command Here
+
+
