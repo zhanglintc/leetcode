2a34e7d38868632976941c29e45aab90486150b9
Lane <zhanglintc623@gmail.com>
2015-07-01 11:22:58 +0800

    AC

--- Shell/Valid Phone Numbers.sh
+++ Shell/Valid Phone Numbers.sh
@@ -25,6 +25,7 @@
 # (123) 456-7890
 
 # Read from the file file.txt and output all valid phone numbers to stdout.
+
 grep -E "^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$" file.txt
 
 
