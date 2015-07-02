02a74514107aabca2bb7f0cbf32d6f024a3036aa
Lane <zhanglintc623@gmail.com>
2015-07-03 00:09:31 +0800

    INIT

---
+++ Python/Combination Sum III.py
@@ -0,0 +1,39 @@
+# Combination Sum III
+# for leetcode problems
+# 2015.07.03 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/combination-sum-iii/
+
+# Problem:
+# Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+# Ensure that numbers within the set are sorted in ascending order.
+
+# Example 1:
+
+# Input: k = 3, n = 7
+
+# Output:
+
+# [[1,2,4]]
+
+# Example 2:
+
+# Input: k = 3, n = 9
+
+# Output:
+
+# [[1,2,6], [1,3,5], [2,3,4]]
+
+# Credits:
+# Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+class Solution:
+    # @param {integer} k
+    # @param {integer} n
+    # @return {integer[][]}
+    def combinationSum3(self, k, n):
+        pass
+
+
