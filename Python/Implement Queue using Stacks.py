7d2c6f12053c7ce8101068bf602562e70838e9eb
Lane <zhanglintc623@gmail.com>
2015-07-09 22:42:43 +0800

    INIT

---
+++ Python/Implement Queue using Stacks.py
@@ -0,0 +1,47 @@
+# Implement Queue using Stacks
+# for leetcode problems
+# 2015.07.09 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/implement-queue-using-stacks/
+
+# Problem:
+# Implement the following operations of a queue using stacks.
+
+# push(x) -- Push element x to the back of queue.
+# pop() -- Removes the element from in front of queue.
+# peek() -- Get the front element.
+# empty() -- Return whether the queue is empty.
+
+# Notes:
+# You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size,
+# and is empty operations are valid.
+# Depending on your language, stack may not be supported natively.
+# You may simulate a stack by using a list or deque (double-ended queue),
+# as long as you use only standard operations of a stack.
+# You may assume that all operations are valid
+# (for example, no pop or peek operations will be called on an empty queue).
+
+class Queue:
+    # initialize your data structure here.
+    def __init__(self):
+        pass
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        pass
+
+    # @return nothing
+    def pop(self):
+        pass
+
+    # @return an integer
+    def peek(self):
+        pass
+
+    # @return an boolean
+    def empty(self):
+        pass
+
+
