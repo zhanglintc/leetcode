5f97a082fc1cd6189510e57fe004fc231e08a2b3
Lane <zhanglintc623@gmail.com>
2015-07-18 12:02:52 +0800

    INIT

---
+++ Python/Delete Node in a Linked List.py
@@ -0,0 +1,26 @@
+# Delete Node in a Linked List
+# for leetcode problems
+# 2015.07.18 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+# Problem:
+# Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+# Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+# the linked list should become 1 -> 2 -> 4 after calling your function.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} node
+    # @return {void} Do not return anything, modify node in-place instead.
+    def deleteNode(self, node):
+        pass
+
+
