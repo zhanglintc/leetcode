0dbb8727709f0ecc09798524f256835fa7b0b04c
Lane <zhanglintc623@gmail.com>
2015-07-25 21:47:44 +0800

    INIT

---
+++ Python/Search a 2D Matrix II.py
@@ -0,0 +1,37 @@
+# Search a 2D Matrix II
+# for leetcode problems
+# 2015.07.25 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+# Problem:
+# Write an efficient algorithm that searches for a value in an m x n matrix.
+
+# This matrix has the following properties:
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+# For example,
+
+# Consider the following matrix:
+
+# [
+#   [1,   4,  7, 11, 15],
+#   [2,   5,  8, 12, 19],
+#   [3,   6,  9, 16, 22],
+#   [10, 13, 14, 17, 24],
+#   [18, 21, 23, 26, 30]
+# ]
+
+# Given target = 5, return true.
+
+# Given target = 20, return false.
+
+class Solution:
+    # @param {integer[][]} matrix
+    # @param {integer} target
+    # @return {boolean}
+    def searchMatrix(self, matrix, target):
+        pass
+
+
