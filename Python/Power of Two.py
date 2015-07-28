8bd568f830344d9192b4aeff282b4d8377352324
Lane <zhanglintc623@gmail.com>
2015-07-28 22:48:46 +0800

    INIT

---
+++ Python/Power of Two.py
@@ -0,0 +1,17 @@
+# Power of Two
+# for leetcode problems
+# 2015.07.28 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/power-of-two/
+
+# Problem:
+# Given an integer, write a function to determine if it is a power of two.
+
+class Solution:
+    # @param {integer} n
+    # @return {boolean}
+    def isPowerOfTwo(self, n):
+        pass
+
+
