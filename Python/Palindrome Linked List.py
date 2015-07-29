22510e0a46cd7b44789f27d3de2fe56d67313bab
Lane <zhanglintc623@gmail.com>
2015-07-29 14:32:04 +0800

    INIT

---
+++ Python/Palindrome Linked List.py
@@ -0,0 +1,26 @@
+# Palindrome Linked List
+# for leetcode problems
+# 2015.07.29 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/palindrome-linked-list/
+
+# Problem:
+# Given a singly linked list, determine if it is a palindrome.
+
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {boolean}
+    def isPalindrome(self, head):
+        pass
+
+
