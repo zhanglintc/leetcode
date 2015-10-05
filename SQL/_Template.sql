c82eb6ce0d4181df4bfa4ab4f31f89afbd15afba
Lane <zhanglintc623@gmail.com>
2015-10-06 00:46:24 +0800

    _Template

--- SQL/_Template.sql
+++ SQL/_Template.sql
@@ -1,12 +1,12 @@
-# Problem Name Here
-# for leetcode problems
-# 2015.10.06 by zhanglin
+-- Problem Name Here
+-- for leetcode problems
+-- 2015.10.06 by zhanglin
 
-# Problem Link:
-# https://leetcode.com/problems/problem-title/
+-- Problem Link:
+-- https://leetcode.com/problems/problem-title/
 
-# Problem:
-# Problem description here
+-- Problem:
+-- Problem description here
 
 Your SQL Command Here
 
