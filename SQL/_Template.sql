c85f9f008b0e7571efedafb377635d5acbe23fe0
Lane <zhanglintc623@gmail.com>
2015-10-06 00:44:13 +0800

    _Template

---
+++ SQL/_Template.sql
@@ -0,0 +1,13 @@
+# Problem Name Here
+# for leetcode problems
+# 2015.10.06 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/problem-title/
+
+# Problem:
+# Problem description here
+
+Your SQL Command Here
+
+
