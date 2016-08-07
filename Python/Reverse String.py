335976d3d3256148cf8bf0435bfa2e7ec65d19a8
Lane <zhanglintc623@gmail.com>
2016-08-07 23:14:13 +0800

    INIT

---
+++ Python/Reverse String.py
@@ -0,0 +1,22 @@
+# Reverse String
+# for leetcode problems
+# 2016.08.07 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/reverse-string/
+
+# Problem:
+# Write a function that takes a string as input and returns the string reversed.
+
+# Example:
+# Given s = "hello", return "olleh".
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        pass
+
+
