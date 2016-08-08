af60c53e6721b6768a2e3f5020c5e4989e187501
zhanglintc <zhanglintc623@gmail.com>
2016-08-08 11:23:44 +0800

    AC

--- Python/Add Digits.py
+++ Python/Add Digits.py
@@ -25,6 +25,12 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        pass
+        if num == 0:
+            return 0
+
+        if num % 9 == 0:
+            return 9
+
+        return num % 9
 
 
