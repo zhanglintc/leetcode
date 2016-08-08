435ea7d04920df868533bada382fc89b500e6e84
zhanglintc <zhanglintc623@gmail.com>
2016-08-08 11:40:09 +0800

    INIT

---
+++ Python/Sum of Two Integers.py
@@ -0,0 +1,23 @@
+# Sum of Two Integers
+# for leetcode problems
+# 2016.08.08 by zhanglin
+
+# Problem Link:
+# https://leetcode.com/problems/sum-of-two-integers/
+
+# Problem:
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        pass
+
+
